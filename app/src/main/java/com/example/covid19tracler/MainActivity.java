b5bfdb7d21e35d7285c36005cce5fe600ccc4159
chathurikaPerera <>
2020-11-24 23:17:33 +0530

    2020-11-24

--- app/src/main/java/com/example/covid19tracler/MainActivity.java
+++ app/src/main/java/com/example/covid19tracler/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -13,9 +15,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import javax.xml.transform.ErrorListener;
 
@@ -23,6 +28,8 @@ import javax.xml.transform.ErrorListener;
    public class MainActivity extends AppCompatActivity {
        private RequestQueue requestQueue;
        private TextView totalCases, avtiveCases, newCases, recoveredCases, deaths;
+       public FloatingActionButton floatingActionButton;
+       ArrayList<String> booo = new ArrayList<String>();
 
 
     @Override
@@ -35,9 +42,19 @@ import javax.xml.transform.ErrorListener;
         newCases = findViewById(R.id.newCases);
         recoveredCases =findViewById(R.id.recoverCases);
         deaths = findViewById(R.id.deathCases);
+        floatingActionButton = findViewById(R.id.floatbutton);
 
         requestQueue = Volley.newRequestQueue(this);
         jsonParse();
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class );
+                intent.putExtra("booolist", booo);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -53,6 +70,14 @@ import javax.xml.transform.ErrorListener;
                        String ncases = obj.getString("local_new_cases");
                        String rcaces = obj.getString("local_recovered");
                        String death = obj.getString("local_deaths");
+                       String gncases = obj.getString("global_new_cases");
+                       String gtcases = obj.getString("global_total_cases");
+                       String gdeaths = obj.getString("global_deaths");
+                       String grecovered = obj.getString("global_recovered");
+                       booo.add(gncases);
+                       booo.add(gtcases);
+                       booo.add(gdeaths);
+                       booo.add(grecovered);
 
                        totalCases.setText(tcases);
                        avtiveCases.setText(acases);
